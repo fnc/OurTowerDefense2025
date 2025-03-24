0f0d79b53442d3dcc9978bc4f39057255a6008a3
Driannn <drianbetancourt@gmail.com>
2025-03-24 18:00:41 -0300

    rename stuff

--- audio/audio_manager.gd
+++ audio/audio_manager.gd
@@ -3,4 +3,4 @@ extends Node
 @export var take_hit: AudioStreamPlayer
 @export var shot: AudioStreamPlayer
 @export var bg_loop: AudioStreamPlayer
-@export var boton: AudioStreamPlayer
+@export var UIClick: AudioStreamPlayer
